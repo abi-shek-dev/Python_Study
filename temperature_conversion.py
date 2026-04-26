7625bb2df599b8c82c9151ce4ffec0ec98459ecf
abi-shek-dev <abishekinterspace@gmail.com>
2026-04-27 02:33:14 +0530

    temperature convertor

---
+++ temperature_conversion.py
@@ -0,0 +1,11 @@
+unit = input("Celsius (C) or Fahrenheit (F)? (C/F): ")
+temp = float(input("Enter the temperature: "))
+
+if unit.upper() == "C":
+    temp = (temp * 9/5) + 32
+    print(f"The temperature in Fahrenheit is: {temp:.2f} F")
+elif unit.upper() == "F":
+    temp = (temp - 32) * 5/9
+    print(f"The temperature in Celsius is: {temp:.2f} C")
+else:
+    print(f"{unit} is not valid.")
