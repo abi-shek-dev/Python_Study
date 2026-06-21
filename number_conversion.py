9e541ad1ec8bc30a5712a1e5fc76e6e252ace8f5
abi-shek-dev <abishekinterspace@gmail.com>
2026-06-21 21:25:42 +0530

    number conversion 2

--- number_conversion.py
+++ number_conversion.py
@@ -3,3 +3,11 @@ n = int(input())
 print("Binary:", bin(n)[2:])
 print("Octal:", oct(n)[2:])
 print("Hexadecimal:", hex(n)[2:])
+
+x = bin(n)
+y = oct(n)
+z = hex(n)
+
+print(int(x, 2))
+print(int(y,8))
+print(int(z,16))
