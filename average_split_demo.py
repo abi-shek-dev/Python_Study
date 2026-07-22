322a418222273abeb52f4982a52a35cab98022e8
abi-shek-dev <abishekinterspace@gmail.com>
2026-07-23 05:18:46 +0530

    avg split demo

--- average_split_demo.py
+++ average_split_demo.py
@@ -1,25 +1,28 @@
 def average_split(arr):
 
     total = sum(arr)
-
     n = len(arr)
-    dp = [set() for _ in range(n+1)]
+
+    dp = [set() for _ in range(n + 1)]
     dp[0].add(0)
 
     for num in arr:
-        for k in range(n -1 , -1 , -1):
-            for s in dp[k]:
-                dp[k+1].add(s+num)
-
-    for k in range(1, n):
-        if total * k % n == 0:
-            target = total * k / n
+        
+        for k in range(n - 1, -1, -1):
+            
+            for s in list(dp[k]):
+                
+                dp[k+1].add(num+s)
+                
+    for k in range(1,n):
+        
+        if total % k == 0:
+            target = total // k
+            
             if target in dp[k]:
                 return True
-
+            
     return False
 
-    
 arr = list(map(int,input().split()))
-
 print(average_split(arr))
