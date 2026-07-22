f0509afc067152ad40c6bd1aae8059e62abffd73
abi-shek-dev <abishekinterspace@gmail.com>
2026-07-23 04:55:11 +0530

    change

--- roman_to_numbers.py
+++ roman_to_numbers.py
@@ -9,9 +9,12 @@ def romanToInt(s):
         "M": 1000
     }
 
-    s = s.replace("IV", "IIII").replace("IX", "VIIII")
-    s = s.replace("XL", "XXXX").replace("XC", "LXXXX")
-    s = s.replace("CD", "CCCC").replace("CM", "DCCCC")
+    s = s.replace("IV", "IIII")
+    s = s.replace("IX", "VIIII")
+    s = s.replace("XL", "XXXX")
+    s = s.replace("XC", "LXXXX")
+    s = s.replace("CD", "CCCC")
+    s = s.replace("CM", "DCCCC")
 
     number = 0
 
