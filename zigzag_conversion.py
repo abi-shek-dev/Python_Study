372f729b95da2a8867e268b512f0798d65c52122
abi-shek-dev <abishekinterspace@gmail.com>
2026-07-23 04:54:53 +0530

    zigzag

---
+++ zigzag_conversion.py
@@ -0,0 +1,23 @@
+def zigzag(s , numrows):
+
+    if numrows == 1 or numrows > len(s):
+        return s
+
+    rows = [""] * numrows
+
+    direction = -1
+    current_row = 0
+
+    for ch in s:
+        rows[current_row] += ch
+
+        if current_row == 0 or current_row == numrows - 1:
+            direction *= -1
+
+        current_row += direction
+
+    return "".join(rows)
+
+s = input()
+numrows = int(input())
+print(zigzag(s,numrows))
